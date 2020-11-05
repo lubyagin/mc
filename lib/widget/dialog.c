lubyagin/mc

commit 2e734e56189e72b5f461ade4ee3eebdc89d8b739

Make keybind map a member of Widget.
Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/widget/dialog.c
@@ -198,7 +198,7 @@ dlg_handle_key (WDialog * h, int d_key)
 {
     long command;
 
-    command = keybind_lookup_keymap_command (dialog_map, d_key);
+    command = widget_lookup_key (WIDGET (h), d_key);
     if (command != CK_IgnoreKey)
         return dlg_execute_cmd (h, command);
 
@@ -462,6 +462,8 @@ dlg_create (gboolean modal, int y1, int x1, int lines, int cols, widget_pos_flag
     w->state |= WST_CONSTRUCT | WST_FOCUSED;
     /* Temporary hack: dialog doesn't have an owner, own itself. */
     w->owner = g;
+
+    w->keymap = dialog_map;
 
     w->mouse_handler = dlg_handle_mouse_event;
     w->mouse.forced_capture = mouse_close_dialog && (w->pos_flags & WPOS_FULLSCREEN) == 0;

lib/widget/buttonbar.c
@@ -44,7 +44,6 @@
 #include "lib/skin.h"
 #include "lib/strutil.h"
 #include "lib/util.h"
-#include "lib/keybind.h"        /* global_keymap_t */
 #include "lib/widget.h"
 
 /*** global variables ****************************************************************************/

lib/widget/dialog.h
@@ -13,7 +13,6 @@
 
 #include "lib/global.h"
 #include "lib/hook.h"           /* hook_t */
-#include "lib/keybind.h"        /* global_keymap_t */
 
 /*** typedefs(not structures) and defined constants **********************************************/
 

lib/widget/input.c
@@ -46,7 +46,6 @@
 #include "lib/skin.h"
 #include "lib/strutil.h"
 #include "lib/util.h"
-#include "lib/keybind.h"        /* global_keymap_t */
 #include "lib/widget.h"
 #include "lib/event.h"          /* mc_event_raise() */
 #include "lib/mcconfig.h"       /* mc_config_history_*() */
@@ -998,6 +997,7 @@ input_new (int y, int x, const int *colors, int width, const char *def_text,
     w = WIDGET (in);
     widget_init (w, y, x, 1, width, input_callback, input_mouse_callback);
     w->options |= WOP_SELECTABLE | WOP_IS_INPUT | WOP_WANT_CURSOR;
+    w->keymap = input_map;
 
     in->color = colors;
     in->first = TRUE;
@@ -1139,8 +1139,7 @@ input_handle_char (WInput * in, int key)
         return v;
     }
 
-    command = keybind_lookup_keymap_command (input_map, key);
-
+    command = widget_lookup_key (WIDGET (in), key);
     if (command == CK_IgnoreKey)
     {
         if (key > 255)
@@ -1174,9 +1173,7 @@ input_key_is_in_map (WInput * in, int key)
 {
     long command;
 
-    (void) in;
-
-    command = keybind_lookup_keymap_command (input_map, key);
+    command = widget_lookup_key (WIDGET (in), key);
     if (command == CK_IgnoreKey)
         return 0;
 

lib/widget/input.h
@@ -8,8 +8,6 @@
 
 #include <limits.h>             /* MB_LEN_MAX */
 
-#include "lib/keybind.h"        /* global_keymap_t */
-
 /*** typedefs(not structures) and defined constants **********************************************/
 
 #define INPUT(x) ((WInput *)(x))

lib/widget/listbox.c
@@ -42,7 +42,6 @@
 #include "lib/skin.h"
 #include "lib/strutil.h"
 #include "lib/util.h"           /* Q_() */
-#include "lib/keybind.h"        /* global_keymap_t */
 #include "lib/widget.h"
 
 /*** global variables ****************************************************************************/
@@ -355,7 +354,7 @@ listbox_key (WListbox * l, int key)
         return MSG_HANDLED;
     }
 
-    command = keybind_lookup_keymap_command (listbox_map, key);
+    command = widget_lookup_key (WIDGET (l), key);
     if (command == CK_IgnoreKey)
         return MSG_NOT_HANDLED;
     return listbox_execute_cmd (l, command);
@@ -559,6 +558,7 @@ listbox_new (int y, int x, int height, int width, gboolean deletable, lcback_fn 
     w = WIDGET (l);
     widget_init (w, y, x, height, width, listbox_callback, listbox_mouse_callback);
     w->options |= WOP_SELECTABLE | WOP_WANT_HOTKEY;
+    w->keymap = listbox_map;
 
     l->list = NULL;
     l->top = l->pos = 0;

lib/widget/listbox.h
@@ -6,8 +6,6 @@
 #ifndef MC__WIDGET_LISTBOX_H
 #define MC__WIDGET_LISTBOX_H
 
-#include "lib/keybind.h"        /* global_keymap_t */
-
 /*** typedefs(not structures) and defined constants **********************************************/
 
 #define LISTBOX(x) ((WListbox *)(x))

lib/widget/menu.c
@@ -39,7 +39,6 @@
 #include "lib/tty/tty.h"
 #include "lib/skin.h"
 #include "lib/tty/key.h"        /* key macros */
-#include "lib/keybind.h"        /* global_keymap_t */
 #include "lib/strutil.h"
 #include "lib/widget.h"
 #include "lib/event.h"          /* mc_event_raise() */
@@ -589,7 +588,7 @@ menubar_handle_key (WMenuBar * menubar, int key)
     unsigned long cmd;
     cb_ret_t ret = MSG_NOT_HANDLED;
 
-    cmd = keybind_lookup_keymap_command (menu_map, key);
+    cmd = widget_lookup_key (WIDGET (menubar), key);
 
     if (cmd != CK_IgnoreKey)
         ret = menubar_execute_cmd (menubar, cmd);
@@ -950,6 +949,7 @@ menubar_new (GList * menu, gboolean visible)
     /* initially, menubar is not selectable */
     widget_set_options (w, WOP_SELECTABLE, FALSE);
     w->options |= WOP_TOP_SELECT;
+    w->keymap = menu_map;
     menubar->is_visible = visible;
     menubar_set_menu (menubar, menu);
 

commit 91a251a522e2defa155cf1ef3ede20f8325ddaa4

Ticket #4142: ctrl-l shortcut (refresh screen) is broken.
This bug was introduced in 2e734e56189e72b5f461ade4ee3eebdc89d8b739.

(dlg_handle_key): fallback to dialog_map if shortcut is not defined in
filemanager/editor/viewer/diffviewer key map.

Signed-off-by: Andrew Borodin <aborodin@vmail.ru>

lib/widget/dialog.c
@@ -206,6 +206,8 @@ dlg_handle_key (WDialog * h, int d_key)
     long command;
 
     command = widget_lookup_key (WIDGET (h), d_key);
+    if (command == CK_IgnoreKey)
+        command = keybind_lookup_keymap_command (dialog_map, d_key);
     if (command != CK_IgnoreKey)
         return dlg_execute_cmd (h, command);
 